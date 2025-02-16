2fc7b1c4a995cd5e3c82ac847dc9944ce1975d1c
edoubart <edouard.barthelemy@gmail.com>
2025-02-16 15:13:43 -0500

    Working Read Logs

--- src/main.rs
+++ src/main.rs
@@ -2,52 +2,35 @@
  * Imports *
  ***********/
 use std::fs;
-use std::io::Error;
+
+fn extract_errors(text: &str) -> Vec<String> {
+    let split_text = text.split("\n");
+
+    let mut results = vec![];
+
+    for line in split_text {
+        if line.starts_with("ERROR") {
+            results.push(line.to_string());
+        }
+    }
+
+    results
+}
 
 /*****************
  * Main Function *
  *****************/
 fn main() {
-    //let text = fs::read_to_string("logs.txt");
+    let mut error_logs = vec![];
 
-    //println!("{:#?}", text)
-
-    match divide(5.0, 0.0) {
-        Ok(result_of_division) => {
-            println!("{}", result_of_division);
+    match fs::read_to_string("logs.txt") {
+        Ok(text_that_was_read) => {
+            error_logs = extract_errors(text_that_was_read.as_str());
         }
-        Err(what_went_wrong) => {
-            println!("{}", what_went_wrong);
+        Err(why_this_failed) => {
+            println!("Failed to read file: {}", why_this_failed);
         }
     }
 
-    match validate_email(String::from("test.test.com")) {
-        Ok(..) => println!("email is valid"),
-        Err(reason_this_failed_validation) => {
-            println!("{}", reason_this_failed_validation)
-        }
-    }
-}
-
-fn validate_email(email: String) -> Result<(), Error> {
-    if email.contains("@") {
-        Ok(()) // Empty Tuple
-    } else {
-        Err(Error::other("emails must have an @ sign"))
-    }
-}
-
-/*
- * Definition of 'Result' in Rust:
- *  enum Result<T, E> {
- *      Ok(T),
- *      Err(E)
- *  }
- */
-fn divide(a: f64, b: f64) -> Result<f64, Error> {
-    if b == 0.0 {
-        Err(Error::other("can't divide by 0"))
-    } else {
-        Ok(a / b)
-    }
+    println!("{:#?}", error_logs);
 }
