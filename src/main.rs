98d86211b0d501ea255fccdfdcf9417699d0cbbd
edoubart <edouard.barthelemy@gmail.com>
2025-02-17 00:20:57 -0500

    Use 'Try (?)' Operator instead.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
  * Imports *
  ***********/
 use std::fs;
+use std::io::Error;
 
 fn extract_errors(text: &str) -> Vec<String> {
     let split_text = text.split("\n");
@@ -20,17 +21,26 @@ fn extract_errors(text: &str) -> Vec<String> {
 /*****************
  * Main Function *
  *****************/
-fn main() {
-    // Condensed version instead
-    let text = fs::read_to_string("logs.txt")
-        .expect("failed to read logs.txt");
+fn main() -> Result<(), Error> {
+    // 'Try' Operator version
+    let text = fs::read_to_string("logs.txt")?;
 
     let error_logs = extract_errors(text.as_str());
 
-    fs::write("errors.txt", error_logs.join("\n"))
-        .expect("failed to write logs.txt");
+    fs::write("errors.txt", error_logs.join("\n"))?;
 
-    // Nested Match Statements
+    Ok(())
+
+    // 'Result.expect(..)' version
+    //let text = fs::read_to_string("logs.txt")
+    //    .expect("failed to read logs.txt");
+
+    //let error_logs = extract_errors(text.as_str());
+
+    //fs::write("errors.txt", error_logs.join("\n"))
+    //    .expect("failed to write logs.txt");
+
+    // Nested Match Statements version
     //match fs::read_to_string("logs.txt") {
     //    Ok(text_that_was_read) => {
     //        let error_logs = extract_errors(text_that_was_read.as_str());
