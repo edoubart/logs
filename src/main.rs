87e98a626cc4fbb129c3ac0e6ecd45a5873676a5
edoubart <edouard.barthelemy@gmail.com>
2025-02-16 23:48:36 -0500

    Use 'Result.expect(..)' instead of convoluted nested matches.

--- src/main.rs
+++ src/main.rs
@@ -21,19 +21,29 @@ fn extract_errors(text: &str) -> Vec<String> {
  * Main Function *
  *****************/
 fn main() {
-    match fs::read_to_string("logs.txt") {
-        Ok(text_that_was_read) => {
-            let error_logs = extract_errors(text_that_was_read.as_str());
+    // Condensed version instead
+    let text = fs::read_to_string("logs.txt")
+        .expect("failed to read logs.txt");
 
-            match fs::write("errors.txt", error_logs.join("\n")) {
-                Ok(..) => println!("Wrote errors.txt"),
-                Err(reason_write_failed) => {
-                    println!("Writing of errors.txt failed: {}", reason_write_failed);
-                }
-            }
-        }
-        Err(why_this_failed) => {
-            println!("Failed to read file: {}", why_this_failed);
-        }
-    }
+    let error_logs = extract_errors(text.as_str());
+
+    fs::write("errors.txt", error_logs.join("\n"))
+        .expect("failed to write logs.txt");
+
+    // Nested Match Statements
+    //match fs::read_to_string("logs.txt") {
+    //    Ok(text_that_was_read) => {
+    //        let error_logs = extract_errors(text_that_was_read.as_str());
+
+    //        match fs::write("errors.txt", error_logs.join("\n")) {
+    //            Ok(..) => println!("Wrote errors.txt"),
+    //            Err(reason_write_failed) => {
+    //                println!("Writing of errors.txt failed: {}", reason_write_failed);
+    //            }
+    //        }
+    //    }
+    //    Err(why_this_failed) => {
+    //        println!("Failed to read file: {}", why_this_failed);
+    //    }
+    //}
 }
